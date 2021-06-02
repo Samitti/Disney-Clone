e0356d17bb162330a03f8591554485761a650ef0
Samitti <samuelteweldebrhan@gmail.com>
2021-06-02 15:40:08 +0300

    add originals trending and newtodisney components to the home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
+import Recommends from "./Recommends";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Trending from "./Trending";
 
 const Home = (props) => {
     return(
         <Container>
             <ImgSlider />
             <Viewers />
+            <Recommends />
+            <NewDisney />
+            <Originals />
+            <Trending />
         </Container>
     );
 };
